11ff7d07863640fc380ea0d6e5cf3d69a21e58df
dhytalu <nurdita07@gmail.com>
2023-08-08 08:21:16 +0700

    link author

--- yanum-arsip.php
+++ yanum-arsip.php
@@ -8,7 +8,7 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              https://github.com/velocitydeveloper/
+ * @link              https://github.com/dhytalu/yanum-arsip-plugin.git
  * @since             1.0.0
  * @package           arsip_yanum
  *
